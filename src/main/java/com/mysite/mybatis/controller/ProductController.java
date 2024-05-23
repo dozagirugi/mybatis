e16c89bac806bc8b8d33cec3dd987d3489b66361
dozagirugi <dozagirugi@gmail.com>
2024-05-23 09:02:13 +0900

    My Batis XML 설정

---
+++ src/main/java/com/mysite/mybatis/controller/ProductController.java
@@ -0,0 +1,54 @@
+package com.mysite.mybatis.controller;
+
+import com.mysite.mybatis.mapper.ProductMapper;
+import com.mysite.mybatis.mapper.UserMapper;
+import com.mysite.mybatis.model.Product;
+import com.mysite.mybatis.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+// RestController는 화면 없이 바로 객체나 문자열을 제이슨으로 리턴한다
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductMapper productMapper;
+
+    @GetMapping("/{id}")
+    public Product getProduct(@PathVariable Long id) {
+        Product product = productMapper.selectProductById(id);
+        return product;
+    }
+
+    @GetMapping
+    public List<Product> getProducts() {
+        List<Product> products = productMapper.selectAllProducts();
+        return products;
+    }
+
+    @PostMapping
+    public void createProduct(@RequestParam("name") String prodName,
+                              @RequestParam("price") int prodPrice) {
+
+        productMapper.insertProduct(new Product(prodName, prodPrice));
+
+    }
+//
+//    @PutMapping("/{id}")
+//    public void editUser(@PathVariable String id,
+//                         @RequestParam("name") String name,
+//                         @RequestParam("phone") String phone,
+//                         @RequestParam("address") String address) {
+//
+//        userMapper.updateUser(id, name, phone, address);
+//    }
+//
+//
+//    @DeleteMapping("/{id}")
+//    public void deleteUser(@PathVariable String id) {
+//        userMapper.deleteUser(id);
+//    }
+}
--- src/main/java/com/mysite/mybatis/mapper/ProductMapper.java
+++ src/main/java/com/mysite/mybatis/mapper/ProductMapper.java
@@ -1,4 +1,18 @@
 package com.mysite.mybatis.mapper;
 
-public class ProductMapper {
+import com.mysite.mybatis.model.Product;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface ProductMapper {
+
+    // XML을 사용할때는 추상메소드만 만들고 실제 SQL은 지정된 폴더의 XML에 적는다
+    Product selectProductById(Long id);
+    List<Product> selectAllProducts();
+    void insertProduct(Product product);
+    void updateProduct(Product product);
+    void deleteProductById(Long id);
+
 }
